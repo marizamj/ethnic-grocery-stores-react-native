eac790ca983dc79790ef1e438f1e7d8cca523d80
Marie <marizamj@gmail.com>
2017-02-11 15:56:49 +0100

    Wrap 'About' into <ScrollView>

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Header from './Header';
 
@@ -17,29 +17,31 @@ export default class About extends Component {
         <View style={styles.iconPlaceholder} />
       </Header>
 
-      <Text style={styles.p}>
-        Hi there!
-        My name is Marie and I created this app
-        because I live in Amsterdam and really love trying
-        and cooking new foods.
-      </Text>
-      <Text style={styles.p}>
-        If you share my passion and know some cool place
-        which is not on this map, please login via Google
-        account and fill the form with it.
-      </Text>
-      <Text style={styles.p}>
-        I created this project while learning how to code.
-        It is powered by React Native and uses Firebase to
-        store data. You also can find (and star ★) it on my
-        Github page.
-      </Text>
-      <View style={[styles.flexRow, { marginTop: 30, justifyContent: 'center' }]}>
-        <Icon style={styles.aboutIcons} name="logo-facebook" />
-        <Icon style={styles.aboutIcons} name="logo-github" />
-        <Icon style={styles.aboutIcons} name="logo-linkedin" />
-        <Icon style={styles.aboutIcons} name="ios-mail" />
-      </View>
+      <ScrollView>
+        <Text style={styles.p}>
+          Hi there!
+          My name is Marie and I created this app
+          because I live in Amsterdam and really love trying
+          and cooking new foods.
+        </Text>
+        <Text style={styles.p}>
+          If you share my passion and know some cool place
+          which is not on this map, please login via Google
+          account and fill the form with it.
+        </Text>
+        <Text style={styles.p}>
+          I created this project while learning how to code.
+          It is powered by React Native and uses Firebase to
+          store data. You also can find (and star ★) it on my
+          Github page.
+        </Text>
+        <View style={[styles.flexRow, { marginTop: 30, justifyContent: 'center' }]}>
+          <Icon style={styles.aboutIcons} name="logo-facebook" />
+          <Icon style={styles.aboutIcons} name="logo-github" />
+          <Icon style={styles.aboutIcons} name="logo-linkedin" />
+          <Icon style={styles.aboutIcons} name="ios-mail" />
+        </View>
+      </ScrollView>
     </View>
   }
 };
